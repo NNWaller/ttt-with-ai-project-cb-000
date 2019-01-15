4d0cfc2e3245116a3e4a5d5c99e67099adecad9a
Natasha Waller <nnwaller@gmail.com>
2019-01-15 16:28:59 +0000

    Automatically backed up by Learn

---
+++ lib/players/computer.rb
@@ -0,0 +1,15 @@
+#Players::Computer
+# def move (board) is the method that 
+#returns the move the computer wants to make
+
+#after player_1 makes a move, take the middle cell if
+#empty. Else make a move based on empty cells in  
+#attempt to win the game rather than just block player
+#This would involve making a move next to the previous
+#move. Seems like there is a need to check the 
+#winning combos to do this.
+
+#If player has made 2 out of the 3 needed to make
+#a winning combination, then attempt to block by  
+#filling in the missing cell. Otherwise make a third 
+#move to complete a win for the computer.
